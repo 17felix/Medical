3065505f7de5726dfcf04e580e2efea6bd36e199
17felix <57413284+17felix@users.noreply.github.com>
2022-02-01 20:30:14 +0300

    Revert "Appointments rebuild"

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,12 +1,8 @@
-@@include('accordion.js')
-@@include('popup_login.js')
-@@include('popup_sigin.js')
-@@include('popup_appointment.js')
+// @@include('font-style.js')
+// @@include('testWebP.js')
+// @@include('hamburger.js')
 
-/*
-@@include('font-style.js')
-@@include('testWebP.js')
-@@include('hamburger.js')
-*/
+
 
 console.log("Use this: npm i --save-dev gulp-uglify-es");
+
